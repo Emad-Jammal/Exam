db74c8cd2313dd1ebaebc8649c828f0288915302
O_M_D_V <emad.elqassas.1@gmail.com>
2023-08-23 02:13:33 +0300

    modification

--- js/main.js
+++ js/main.js
@@ -4,15 +4,19 @@ $(window).scroll(()=>{
   let windowScroll = $(window).scrollTop();
 
   if (windowScroll > aboutTop - 500) {
-    $("nav").css({"background-color":"white","transition":"0.7s all"})
-    $("nav a").css({"color" : "black"})
-    let anchor = $(".anchor");
-    for (let i = 0; i < anchor.length; i++) {
-      let beforeElement = window.getComputedStyle(anchor[i],'::before');
-      anchor[i].style.setProperty("--bg-color","black")
+    $("#bar").css({color: "black" })
+    if ($(window).width > "768px") {
+      $("nav").css({"background-color":"white","transition":"0.7s all"})
+      $("nav a").css({"color" : "black"})
+      let anchor = $(".anchor");
+      for (let i = 0; i < anchor.length; i++) {
+        let beforeElement = window.getComputedStyle(anchor[i],'::before');
+        anchor[i].style.setProperty("--bg-color","black")
+      }
     }
   
   }else {
+    $("#bar").css({color: "white" })
     $("nav").css({"background-color":"transparent","transition":"0.7s all"});
     $("nav a").css({"color" : "white"})
     let anchor = $(".anchor");
@@ -21,5 +25,4 @@ $(window).scroll(()=>{
       anchor[i].style.setProperty("--bg-color","white")
     }
   }
-
 })
