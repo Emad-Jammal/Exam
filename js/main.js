c931fd363620b4b064c53eb197f11e955d63398a
O_M_D_V <emad.elqassas.1@gmail.com>
2023-10-06 20:05:10 +0300

    Exam

--- js/main.js
+++ js/main.js
@@ -1,28 +1,26 @@
-
-$(window).scroll(()=>{ 
+$(window).scroll(() => {
   let aboutTop = $("#about").offset().top;
   let windowScroll = $(window).scrollTop();
 
   if (windowScroll > aboutTop - 500) {
-    $("#bar").css({color: "black" })
+    $("#bar").css({ color: "black" });
     if ($(window).width > "768px") {
-      $("nav").css({"background-color":"white","transition":"0.7s all"})
-      $("nav a").css({"color" : "black"})
+      $("nav").css({ "background-color": "white", transition: "0.7s all" });
+      $("nav a").css({ color: "black" });
       let anchor = $(".anchor");
       for (let i = 0; i < anchor.length; i++) {
-        let beforeElement = window.getComputedStyle(anchor[i],'::before');
-        anchor[i].style.setProperty("--bg-color","black")
+        let beforeElement = window.getComputedStyle(anchor[i], "::before");
+        anchor[i].style.setProperty("--bg-color", "black");
       }
     }
-  
-  }else {
-    $("#bar").css({color: "white" })
-    $("nav").css({"background-color":"transparent","transition":"0.7s all"});
-    $("nav a").css({"color" : "white"})
+  } else {
+    $("#bar").css({ color: "white" });
+    $("nav").css({ "background-color": "transparent", transition: "0.7s all" });
+    $("nav a").css({ color: "white" });
     let anchor = $(".anchor");
     for (let i = 0; i < anchor.length; i++) {
-      let beforeElement = window.getComputedStyle(anchor[i],'::before');
-      anchor[i].style.setProperty("--bg-color","white")
+      let beforeElement = window.getComputedStyle(anchor[i], "::before");
+      anchor[i].style.setProperty("--bg-color", "white");
     }
   }
-})
+});
